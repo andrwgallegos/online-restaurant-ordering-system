71b805ca59f0bf06aac0e3e299e3d4ea6b437470
andrwgallegos <andrewgallegos60@gmail.com>
2026-04-16 00:27:47 -0400

    refactor: remove duplicate root files and keep api structure

--- models/model_loader.py
+++
@@ -1,6 +0,0 @@
-from . import order_details, orders, recipes, resources, sandwiches
-from ..dependencies.database import Base, engine
-
-
-def index():
-    Base.metadata.create_all(bind=engine)
--- routers/index.py
+++
@@ -1,6 +0,0 @@
-from . import order_details, orders
-
-
-def load_routes(app):
-    app.include_router(orders.router)
-    app.include_router(order_details.router)
